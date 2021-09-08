4b80b424e6d03dbdffba51453e6967deb85bf3ed
Osneil Drakes <osneil-drakes@hotmail.com>
2021-09-08 16:58:42 +0100

    testing in Capybara

--- battle-program/spec/features/testing_infrastructure_spec.rb
+++ battle-program/spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,17 @@
-feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
+# feature 'Testing infrastructure' do
+#   scenario 'Can run app and check page content' do
+#     visit('/')
+#     expect(page).to have_content 'Testing infrastructure working!'
+#   end
+# end
+
+feature 'Enter names' do
+  scenario 'submits names' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in :player_1_name, with:("John")
+    fill_in :player_2_name, with:("Mark")
+    click_button("Submit")
+    save_and_open_page
+    expect(page).to have_content("John vs Mark")
   end
 end
