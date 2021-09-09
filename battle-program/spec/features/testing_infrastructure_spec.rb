f7a1cabdf5da15b1e40dc87a5baf064f7b998b46
Osneil Drakes <osneil-drakes@hotmail.com>
2021-09-09 08:51:43 +0100

    refacort Post request, render a view using a Post-redirect loop

--- battle-program/spec/features/testing_infrastructure_spec.rb
+++ battle-program/spec/features/testing_infrastructure_spec.rb
@@ -1,17 +1,7 @@
-# feature 'Testing infrastructure' do
-#   scenario 'Can run app and check page content' do
-#     visit('/')
-#     expect(page).to have_content 'Testing infrastructure working!'
-#   end
-# end
-
-feature 'Enter names' do
-  scenario 'submits names' do
+feature 'Testing infrastructure' do
+  scenario 'Can run app and check page content' do
     visit('/')
-    fill_in :player_1_name, with:("John")
-    fill_in :player_2_name, with:("Mark")
-    click_button("Submit")
-    save_and_open_page
-    expect(page).to have_content("John vs Mark")
+    expect(page).to have_content 'Testing infrastructure working!'
   end
 end
+
