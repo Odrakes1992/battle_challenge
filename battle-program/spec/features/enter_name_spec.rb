d9d58656256b7fd4189fa6c7b8ace1b937890e9a
Osneil Drakes <osneil-drakes@hotmail.com>
2021-09-09 09:14:06 +0100

    refactoring sign in code into separate ruby file with sign in method

--- battle-program/spec/features/enter_name_spec.rb
+++ battle-program/spec/features/enter_name_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submits names' do
-    visit('/')
-    fill_in :player_1_name, with:("Goku")
-    fill_in :player_2_name, with:("Vegeta")
-    click_button("Submit")
+    sign_in_and_play
     #save_and_open_page
     expect(page).to have_content("Goku vs Vegeta")
   end
